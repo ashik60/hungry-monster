7ad975c49f2ad898a72c2847dc0879e017cb6013
Ashik <ashikshaheen187@gmail.com>
2021-02-07 14:52:02 +0600

    finished assignment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# hungry-monster" 
+# hungry-monster
+Live link :
+ https://ashiksh.github.io/hungry-monster/
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Handle search
 const searchMeal = () => {
   const foodInput = document.getElementById("foodInput").value;
   const api = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${foodInput}`;
@@ -6,20 +7,24 @@ const searchMeal = () => {
     .then((data) => displayMeals(data));
 };
 
+// Display Search result
 const displayMeals = (foods) => {
+  //  Hide food details if displaying
+  document.getElementById("ingredients").style.display = "none";
   const foodListDiv = document.getElementById("foodListDiv");
   const meals = foods.meals;
 
   if (meals === null) {
     foodListDiv.innerHTML = "<h3>No matching food found.</h3>";
   } else {
+    foodListDiv.innerHTML = "";
     meals.forEach((meal) => {
       const foodDiv = document.createElement("div");
       const foodId = meal.idMeal;
 
       const foodInfo = `
-            <div class="m-2 bg-light" onclick=displayIngredients('${foodId}')>
-                <img src="${meal.strMealThumb}" class=""/>
+            <div class="m-2 bg-light" onclick=displayFoodDetails('${foodId}')>
+                <img src="${meal.strMealThumb}" class="food-image"/>
                 <h6 class="text-center pb-2">${meal.strMeal}</h6>
             </div>
         `;
@@ -30,14 +35,16 @@ const displayMeals = (foods) => {
   }
 };
 
-const displayIngredients = (id) => {
+// Display food details
+const displayFoodDetails = (id) => {
   const api = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
   fetch(api)
     .then((res) => res.json())
-    .then((data) => displayFoodDetails(data));
+    .then((data) => displayIngredients(data));
 };
 
-const displayFoodDetails = (food) => {
+// Display Ingredient
+const displayIngredients = (food) => {
   document.getElementById("ingredients").style.display = "block";
   const mealImage = document.getElementById("mealImage");
   const mealName = document.getElementById("mealName");
@@ -49,9 +56,11 @@ const displayFoodDetails = (food) => {
   for (let i = 1; i < 10; i++) {
     const ingredientItem = food.meals[0][`strIngredient${i}`];
     const ingredientAmount = food.meals[0][`strMeasure${i}`];
-    const li = document.createElement("li");
-    li.innerHTML = `${ingredientAmount} ${ingredientItem}`;
-    ingredientList.appendChild(li);
-    console.log(ingredientItem);
+    if (ingredientItem) {
+      const li = document.createElement("li");
+      li.innerHTML = `${ingredientAmount} ${ingredientItem}`;
+      ingredientList.appendChild(li);
+      console.log(ingredientItem);
+    }
   }
 };
